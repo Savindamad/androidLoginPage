5d2cad4726bd157c96a9d9201a431d6b53923c9c
savinda <savindamaddd@gmail.com>
2016-08-11 13:03:25 +0530

    modified login.php file

--- login.php
+++ login.php
@@ -1,8 +1,8 @@
 <?php
     $con = mysqli_connect("br-cdbr-azure-south-b.cloudapp.net", "b50735a87d1621", "8a720e5f", "smart_rms");
     
-    $username = $_POST["username"];
-    $password = $_POST["password"];
+    $username = "waiter1";
+    $password = "w123";
 
     $sql_query = "select user_id from user_account where username like '$username' and password like '$password';";
 
