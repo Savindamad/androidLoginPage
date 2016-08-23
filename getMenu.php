d764a813293152c4dba817cd8a8f3cd0d57939b4
savinda <savindamaddd@gmail.com>
2016-08-23 20:17:55 +0530

    getMenu test 4

--- getMenu.php
+++ getMenu.php
@@ -3,8 +3,6 @@
 $con = mysqli_connect("br-cdbr-azure-south-b.cloudapp.net", "b50735a87d1621", "8a720e5f", "smart_rms");
 $sql_query = "select * from menu_items; ";
 
-$result = mysqli_query($con,$sql_query);
-$num_of_rows = mysqi_num_rows($result);
 
 
 
