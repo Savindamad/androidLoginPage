f7d9295c6dab36fbdc9fd5918a9adfb82fa90156
savinda <savindamaddd@gmail.com>
2016-08-23 19:57:36 +0530

    getMenu test 2

--- getMenu.php
+++ getMenu.php
@@ -8,12 +8,7 @@ $num_of_rows = mysqi_num_rows($result);
 
 $temp_array = array();
 
-if($num_of_rows>0){
-	while($row=mysqli_fetch_assoc($result)){
-		$temp_array[] = $row;
-		echo $row;
-	}
-}
+
 
 
 ?>
