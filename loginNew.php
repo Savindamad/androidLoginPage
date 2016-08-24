8c51d5b106ba52a00f7c18f7cdffc5d68b4ed0a7
savinda <savindamaddd@gmail.com>
2016-08-24 13:58:00 +0530

    loginNew test3

--- loginNew.php
+++ loginNew.php
@@ -7,7 +7,7 @@ header('Content-Type: application/json');
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-$sql_query = "select user_id from user_account where username='$username' and password='$password';";
+$sql_query = "select user_id,f_name,l_name from user_account where username='$username' and password='$password';";
 $result = mysqli_query($con,$sql_query);
 $num_of_rows = mysqli_num_rows($result);
 
