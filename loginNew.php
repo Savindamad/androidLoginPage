011a105390f755352843ef5a7fa05b18d1563e2b
savinda <savindamaddd@gmail.com>
2016-08-24 11:53:44 +0530

    logingNew test2

--- loginNew.php
+++ loginNew.php
@@ -1,22 +1,24 @@
 <?php
 $con = mysqli_connect("br-cdbr-azure-south-b.cloudapp.net", "b50735a87d1621", "8a720e5f", "smart_rms");
 
+header('Content-Type: application/json');
+
+
 $username = $_POST["username"];
 $password = $_POST["password"];
 
-$sql_query = "select user_id from user_account where username like '$username' and password like '$password';";
+$sql_query = "select user_id from user_account where username='$username' and password='$password';";
 $result = mysqli_query($con,$sql_query);
 $num_of_rows = mysqli_num_rows($result);
 
 if($num_of_rows>0){
 	$row=mysqli_fetch_assoc($result);
-
-	header('Content-Type: application/json');
-	echo json_encode($row);
+	$json["success"]=$row;
+	echo json_encode($json);
 }
 else{
-	header('Content-Type: application/json');
-	echo json_encode("login fail");
+	$json["fail"]='login fail';
+	echo json_encode($json);
 }
 
 ?>
