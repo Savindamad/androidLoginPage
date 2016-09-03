90eeba813a5e082b85dc09cfcc5b50b6869d9c37
savinda <savindamaddd@gmail.com>
2016-09-04 00:01:05 +0530

    change get menu php

--- getMenu.php
+++ getMenu.php
@@ -1,6 +1,6 @@
 <?php
 $con = mysqli_connect("br-cdbr-azure-south-b.cloudapp.net", "b50735a87d1621", "8a720e5f", "smart_rms");
-$sql_query = "select * from menu_items; ";
+$sql_query = "select * from menu_item; ";
 
 $result = mysqli_query($con,$sql_query);
 $num_of_rows = mysqli_num_rows($result);
