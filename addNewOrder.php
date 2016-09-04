2a9eda8889782a84fd290aa5c5b09163e58bdf5d
savinda <savindamaddd@gmail.com>
2016-09-04 18:25:58 +0530

    test 1

--- addNewOrder.php
+++ addNewOrder.php
@@ -1,5 +1,5 @@
 <?php
-	$con = mysqli_connect("br-cdbr-azure-south-b.cloudapp.net", "b50735a87d1621", "8a720e5f", "smart_rms");
+	/*$con = mysqli_connect("br-cdbr-azure-south-b.cloudapp.net", "b50735a87d1621", "8a720e5f", "smart_rms");
 	header('Content-Type: application/json');
 
 	$sql_query= "SELECT MAX("order_no") AS ("primary key") FROM ("customer_order");";
@@ -12,5 +12,6 @@
 	}
 	else{
 		echo "error";
-	}
+	}*/
+	echo "error";
 ?>
