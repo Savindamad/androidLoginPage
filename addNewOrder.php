2125c7e8f57832722b7cb0f39aeb471fd95f4b3d
savinda <savindamaddd@gmail.com>
2016-09-04 18:30:25 +0530

    test 4

--- addNewOrder.php
+++ addNewOrder.php
@@ -2,9 +2,10 @@
 	$con = mysqli_connect("br-cdbr-azure-south-b.cloudapp.net", "b50735a87d1621", "8a720e5f", "smart_rms");
 	header('Content-Type: application/json');
 
-	$sql_query= "SELECT MAX("order_no") AS ("primary key") FROM ("customer_order");";
+	//$sql_query= "SELECT MAX("order_no") AS ("primary key") FROM ("customer_order");";
+	$sql_query = "select * from menu_item; ";
 	$result = mysqli_query($con,$sql_query);
-	
+
 	/*
 	$num_of_rows = mysqli_num_rows($result);
 	if($num_of_rows>0){
