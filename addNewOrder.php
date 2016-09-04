536985696efa41fce479d8ceaee78df001e08443
savinda <savindamaddd@gmail.com>
2016-09-04 19:09:55 +0530

    test 16

--- addNewOrder.php
+++ addNewOrder.php
@@ -5,14 +5,14 @@
 	//$sql_query= "select max("order_no") as ("primary key") from ("customer_order");";
 	//ql_query = "SELECT * from customer_order ORDER BY order_no DESC LIMIT 0,1;";
 
-	sql_query = "SELECT order_no FROM customer_order WHERE order_no = (SELECT MAX(order_no) FROM customer_order);";
+	$sql_query = "SELECT order_no FROM customer_order WHERE order_no = (SELECT MAX(order_no) FROM customer_order);";
 	//$sql_query = "select * from menu_item; ";
 	$result = mysqli_query($con,$sql_query);
 
 	$num_of_rows = mysqli_num_rows($result);
 	if($num_of_rows>0){
 		$row=mysqli_fetch_assoc($result);
-		echo $row;
+		echo $row[0];
 	}
 	else{
 		echo "error";
