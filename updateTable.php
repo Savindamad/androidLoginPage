9fc51cee2dea615e791dd58f7833d39abd3681e0
savinda <savindamaddd@gmail.com>
2016-09-04 11:14:58 +0530

    test4

--- updateTable.php
+++ updateTable.php
@@ -1,7 +1,8 @@
 <?php
 	$con = mysqli_connect("br-cdbr-azure-south-b.cloudapp.net", "b50735a87d1621", "8a720e5f", "smart_rms");
     header('Content-Type: application/json');
-    $userID = $_POST["userID"];
+
+    $userID = "3";
     $tableNum = $_POST["tableNum"];
 
 	$sql_query = "update table_type set waiter_id='$userID' where table_no='$tableNum';";
