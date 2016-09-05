c0ea3cb5fdc30b538a4d8d11a254e3cd84dd84dc
savinda <savindamaddd@gmail.com>
2016-09-05 14:54:15 +0530

    test2

--- addNewOrder.php
+++ addNewOrder.php
@@ -6,7 +6,7 @@
 	//$sql_query = "SELECT * from customer_order ORDER BY order_no DESC LIMIT 0,1;";
 
 	//$sql_query = "select max(order_no) from customer_order;";
-	$sql_query = "SELECT MAX(order_no) FROM customer_order;";// SELECT LAST_INSERT_ID();";
+	$sql_query = "SELECT LAST_INSERT_ID();";
 	//$sql_query = "select * from customer_order; ";
 
 	$temp_array = array();
