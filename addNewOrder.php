fb0cb67d3869473eef149ba1cca52462fc31ba59
savinda <savindamaddd@gmail.com>
2016-09-05 14:49:28 +0530

    test

--- addNewOrder.php
+++ addNewOrder.php
@@ -6,13 +6,18 @@
 	//$sql_query = "SELECT * from customer_order ORDER BY order_no DESC LIMIT 0,1;";
 
 	//$sql_query = "select max(order_no) from customer_order;";
-	$sql_query = "SELECT MAX(order_no) FROM customer_order;";// SELECT LAST_INSERT_ID();";
+	//$sql_query = "SELECT MAX(order_no) FROM customer_order;";// SELECT LAST_INSERT_ID();";
+	$sql_query = "select * from customer_order; ";
+
+	$temp_array = array();
+
 	$result = mysqli_query($con,$sql_query);
     $num_of_rows = mysqli_num_rows($result);
-    $num = mysqli_fetch_assoc($result);
-    $number = $num['order_no'];
-   
-    echo $number;
- 
 
+    if($num_of_rows>0){
+		while($row=mysqli_fetch_assoc($result)){
+		$temp_array[] = $row;
+		}
+	}
+echo json_encode(array("menu_items"=>$temp_array));
 ?>
