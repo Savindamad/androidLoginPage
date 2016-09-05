43a424a79d56cc2c187532b718e0bc9a305f567b
savinda <savindamaddd@gmail.com>
2016-09-05 13:50:01 +0530

    test2

--- addNewOrder.php
+++ addNewOrder.php
@@ -3,14 +3,16 @@
 	header('Content-Type: application/json');
 
 	//$sql_query= "select max("order_no") as ("primary key") from ("customer_order");";
-	$sql_query = "SELECT * from customer_order ORDER BY order_no DESC LIMIT 0,1;";
+	//$sql_query = "SELECT * from customer_order ORDER BY order_no DESC LIMIT 0,1;";
 
-	//$sql_query = "select max(order_no) from customer_order;";
+	$sql_query = "select max(order_no) from customer_order;";
 	$result = mysqli_query($con,$sql_query);
 
 	while($row = mysqli_fetch_array($result)){
-		"<tr>"
-			"<td>" <?php echo $row['order_no'] ?> "</td>"
-		"</tr>"
+	?>
+		<tr>
+			<td> <?php echo $row['order_no'] ?> </td>
+		</tr>
+	<?php
 	}
 ?>
