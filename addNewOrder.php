16120690895d179b76ff30840eaeb93146d7921d
savinda <savindamaddd@gmail.com>
2016-09-06 23:32:11 +0530

    update addNewOrder php file

--- addNewOrder.php
+++ addNewOrder.php
@@ -2,22 +2,26 @@
 	$con = mysqli_connect("br-cdbr-azure-south-b.cloudapp.net", "b50735a87d1621", "8a720e5f", "smart_rms");
 	header('Content-Type: application/json');
 
-	//$sql_query= "select max("order_no") as ("primary key") from ("customer_order");";
-	//$sql_query = "SELECT * from customer_order ORDER BY order_no DESC LIMIT 0,1;";
+	$user_id = $_POST["userId"];
+	$table_no = $_POST["tableNum"];
 
-	//$sql_query = "select max(order_no) from customer_order;";
-	$sql_query = "insert into customer_order (cust_name) values ('vvhvsjhv'); select max(order_no) from customer_order;";
-	//$sql_query = "select * from customer_order; ";
+	$sql_query = "insert into customer_order (table_no,ordered_by,accepted) values ('$table_no','$user_id',0);";
+	$sql_query1 = "select * from customer_order where order_no=(select MAX(order_no) from customer_order);";
 
-	$temp_array = array();
 
 	$result = mysqli_query($con,$sql_query);
-    $num_of_rows = mysqli_num_rows($result);
+	$result1 = mysqli_query($con,$sql_query1);
+
+    $num_of_rows = mysqli_num_rows($result1);
 
     if($num_of_rows>0){
-		while($row=mysqli_fetch_assoc($result)){
-		$temp_array[] = $row;
-		}
+    	$row=mysqli_fetch_assoc($result1);
+    	$json["result"]=$row;
+    	echo json_encode($row);
+	}
+	else{
+		$json["error"]='Can not access database';
+        echo json_encode($json);
 	}
 echo json_encode(array("menu_items"=>$temp_array));
 ?>
