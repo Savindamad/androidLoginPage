2d46f3f0b2cc6ecb3d0b790c35f31caf13184a96
savinda <savindamaddd@gmail.com>
2016-09-10 11:18:13 +0530

    test1

--- addNewOrderItem.php
+++ addNewOrderItem.php
@@ -4,5 +4,33 @@ $con = mysqli_connect("br-cdbr-azure-south-b.cloudapp.net", "b50735a87d1621", "8
 
 header('Content-Type: application/json');
 
+//$json = $_POST["order"];
+$json = array(
+    array(
+        "region" => "valore",
+        "price" => "valore2"
+    ),
+    array(
+        "region" => "valore",
+        "price" => "valore2"
+    ),
+    array(
+        "region" => "valore",
+        "price" => "valore2"
+    )
+);
+
+$jsonIterator = new RecursiveIteratorIterator(
+    new RecursiveArrayIterator(json_decode($json, TRUE)),
+    RecursiveIteratorIterator::SELF_FIRST);
+
+foreach ($jsonIterator as $key => $val) {
+    if(is_array($val)) {
+        echo "$key:\n";
+    } else {
+        echo "$key => $val\n";
+    }
+}
+
 
 ?>
