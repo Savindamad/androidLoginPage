6d131c1c01c7d9a8f302c0e9f12095951388f036
savinda <savindamaddd@gmail.com>
2016-09-10 15:48:26 +0530

    test6

--- addNewOrderItem.php
+++ addNewOrderItem.php
@@ -22,12 +22,16 @@ $json = array(
 echo json_encode($json);
 
 $jsonIterator = new RecursiveIteratorIterator(new RecursiveArrayIterator(json_decode($json, TRUE)),RecursiveIteratorIterator::SELF_FIRST);
+echo json_encode($json);
 
 foreach ($jsonIterator as $key => $val) {
+	echo json_encode($json);
     if(is_array($val)) {
+    	echo json_encode($json);
         echo json_encode($key);
     } else {
         echo json_encode($val);
+        echo json_encode($json);
     }
 }
 
