af3d0c19dfb337583b67d21a6c07b7d6906f5642
savinda <savindamaddd@gmail.com>
2016-09-10 20:59:03 +0530

    test1

--- addNewOrderItem.php
+++ addNewOrderItem.php
@@ -9,16 +9,17 @@ $json = {{"order_id":"1161","item_code":"2","item_qty":2},{"order_id":"1161","it
 
 echo json_encode($json);
 
-$task_array = json_decode($json,true);
-$arraySize = count($task_array);
+//$task_array = json_decode($json,true);
+//$arraySize = count($task_array);
 
-for($i=0; $i<$arraySize; $i++){
+/*for($i=0; $i<$arraySize; $i++){
 	$item_code = $json[$i]["item_code"];
 	$item_qty = $json[$i]["item_qty"];
 	$order_id = $json[$i]["order_id"];
 	$sql_query = "insert into order_item (item_id,order_no,quantity) values ('$item_code','$order_id','$item_qty');";
 	$result = mysqli_query($con,$sql_query);
 }
+*/
 
 	echo json_encode(array("order"=>"successfully"));
 ?>
