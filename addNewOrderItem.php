5c866a67eee496fec6749e3a850a11d0bb52cf5e
savinda <savindamaddd@gmail.com>
2016-09-10 11:20:31 +0530

    test 2

--- addNewOrderItem.php
+++ addNewOrderItem.php
@@ -2,7 +2,7 @@
 
 $con = mysqli_connect("br-cdbr-azure-south-b.cloudapp.net", "b50735a87d1621", "8a720e5f", "smart_rms");
 
-header('Content-Type: application/json');
+//header('Content-Type: application/json');
 
 //$json = $_POST["order"];
 $json = array(
