5f02fc7f610379509c943eb715cabfb7345bbb95
savinda <savindamaddd@gmail.com>
2016-09-10 19:37:05 +0530

    test 1 with android

--- addNewOrderItem.php
+++ addNewOrderItem.php
@@ -2,44 +2,22 @@
 
 $con = mysqli_connect("br-cdbr-azure-south-b.cloudapp.net", "b50735a87d1621", "8a720e5f", "smart_rms");
 
-
-
-//$json = $_POST["order"];
-$json = array(
-    array(
-        "region" => "valore",
-        "price" => "valore2"
-    ),
-    array(
-        "region" => "valore",
-        "price" => "valore2"
-    ),
-    array(
-        "region" => "valore",
-        "price" => "valore2"
-    )
-);
-
-echo json_encode($json[0]["region"]);
-
-$jsonIterator = new RecursiveIteratorIterator(new RecursiveArrayIterator(json_decode($json, TRUE)),RecursiveIteratorIterator::SELF_FIRST);
-foreach ($jsonIterator as $key => $val) {
-	echo json_encode($json);
-    if(is_array($val)) {
-    	echo json_encode($json);
-        echo json_encode($key);
-    } else {
-        echo json_encode($val);
-        echo json_encode($json);
-    }
-}
 header('Content-Type: application/json');
-echo json_encode($json);
 
+$json = $_POST["order"];
 
-echo json_encode($json);
+$task_array = json_decode($json,true);
+$arraySize = count($task_array);
 
+for($i=0; $i<$arraySize; $i++){
+	$item_code = $json[$i]["item_code"];
+	$item_qty = $json[$i]["item_qty"];
+	$order_id = $json[$i]["order_id"];
+	$sql_query = "insert into order_item (item_id,order_no,quantity) values ('$item_code','$order_id','$item_qty');";
+	$result = mysqli_query($con,$sql_query);
+}
 
-
+	$json1["success"]='successfully added';
+	echo json_encode($json);
 
 ?>
