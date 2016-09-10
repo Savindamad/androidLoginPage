397d044cb466c13122e21a30f9089d76d5f3fb24
savinda <savindamaddd@gmail.com>
2016-09-10 21:00:32 +0530

    test2

--- addNewOrderItem.php
+++ addNewOrderItem.php
@@ -5,21 +5,21 @@ $con = mysqli_connect("br-cdbr-azure-south-b.cloudapp.net", "b50735a87d1621", "8
 header('Content-Type: application/json');
 
 //$json = $_POST["order"];
-$json = {{"order_id":"1161","item_code":"2","item_qty":2},{"order_id":"1161","item_code":"58","item_qty":2},{"order_id":"1161","item_code":"1","item_qty":1}};
+$json = {[{"order_id":"1161","item_code":"2","item_qty":2},{"order_id":"1161","item_code":"58","item_qty":2},{"order_id":"1161","item_code":"1","item_qty":1}]};
 
 echo json_encode($json);
 
-//$task_array = json_decode($json,true);
-//$arraySize = count($task_array);
+$task_array = json_decode($json,true);
+$arraySize = count($task_array);
 
-/*for($i=0; $i<$arraySize; $i++){
+for($i=0; $i<$arraySize; $i++){
 	$item_code = $json[$i]["item_code"];
 	$item_qty = $json[$i]["item_qty"];
 	$order_id = $json[$i]["order_id"];
 	$sql_query = "insert into order_item (item_id,order_no,quantity) values ('$item_code','$order_id','$item_qty');";
 	$result = mysqli_query($con,$sql_query);
 }
-*/
+
 
 	echo json_encode(array("order"=>"successfully"));
 ?>
