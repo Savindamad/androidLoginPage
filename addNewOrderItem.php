0ad94921a2223ae6945ac609715df6964e1f07a6
savinda <savindamaddd@gmail.com>
2016-09-11 14:34:37 +0530

    fixed error and test

--- addNewOrderItem.php
+++ addNewOrderItem.php
@@ -56,15 +56,11 @@ if($num_of_rows>0){
 	while($row=mysqli_fetch_assoc($result1)){
 		$temp_array1 = array();
 		$orderNo = $row['order_no'];
-		echo json_encode($orderNo);
 		$sql_query2 =  "select * from order_item where order_no = '$orderNo';";
-		echo json_encode('\n');
 		$result2 = mysqli_query($con,$sql_query2);
 		$num_of_rows1 = mysqli_num_rows($result2);
 		if($num_of_rows1>0){
-
 			while($row1=mysqli_fetch_assoc($result2)){
-				echo json_encode($row1);
 				$temp_array1[] = $row1;
 			}
 			$temp_array[] = $temp_array1;
