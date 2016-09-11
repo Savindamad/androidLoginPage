91d23a8d00ac787979becf4b333a403150519e39
savinda <savindamaddd@gmail.com>
2016-09-11 10:45:19 +0530

    test 1

--- addNewOrderItem.php
+++ addNewOrderItem.php
@@ -42,7 +42,7 @@ for($i=0; $i<$arraySize; $i++){
 	$result = mysqli_query($con,$sql_query);
 }
 
-$sql_query1 = "select * from customer_order where table_no = '$table_no' and accepted != 4;";
+$sql_query1 = "select order_no from customer_order where table_no = '$table_no' and accepted != 4;";
 
 $result1 = mysqli_query($con,$sql_query1);
 $num_of_rows = mysqli_num_rows($result1);
@@ -52,13 +52,20 @@ $temp_array = array();
 if($num_of_rows>0){
 	
 	while($row=mysqli_fetch_assoc($result1)){
-		$temp_array[] = $row;
+		$temp_array1 = array();
+		$orderNo = $row['order_no'];
+		$sql_query2 =  "select * from oder_item where order_no = '$orderNo';";
+		$result2 = mysqli_query($con,$sql_query2);
+		while($row1=mysqli_fetch_assoc($result2)){
+			$temp_array1[] = $row1;
+		}
+		$temp_array[] = $temp_array1;
 	}
 }
 
 //	echo json_encode(array("order"=>$json);
 
-echo json_encode(array("orders"=>$temp_array));
+echo json_encode(array("orders"=>$temp_array1));
 
 
 ?>
