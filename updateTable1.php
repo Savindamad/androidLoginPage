60f5346fd366fe32febd52a48d9a2f4a46abf1a4
savinda <savindamaddd@gmail.com>
2016-09-13 21:45:22 +0530

    update updateTable1.php

--- updateTable1.php
+++ updateTable1.php
@@ -5,11 +5,12 @@
     $tableNum = $_POST["tableNum"];
 
 	$sql_query = "update table_type set waiter_id='0' where table_no='$tableNum';";
-	$sql_query1 = "update customer_order set status='3' where table_no='$tableNum';"
+	$sql_query1 = "update customer_order set status='3' where table_no='$tableNum' and status!='9';"
 
 	$result = mysqli_query($con,$sql_query);
 	$result1 = mysqli_query($con,$sql_query1);
     $num_of_rows = mysqli_num_rows($result);
+    $num_of_rows1 = mysqli_num_rows($result1);
 
     $json["success"]="true";
     echo json_encode($json);
